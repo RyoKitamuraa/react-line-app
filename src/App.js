3f1ec7c98b5ed224805765d8468f5ec99b780700
RyoKitamuraa <97265411+RyoKitamuraa@users.noreply.github.com>
2022-10-14 14:43:02 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import SignIn from './components/SignIn';
+import Line from './components/Line';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {auth} from './firebase'
+
+function App() {
+  const [user] = useAuthState(auth);
+  return (
+    <div>
+      {user ? <Line></Line> : <SignIn></SignIn>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+﻿import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyA7BSWV7yGin4P4mGwZt9JxcZP__O8f_nk",
+  authDomain: "line-clone-5482a.firebaseapp.com",
+  projectId: "line-clone-5482a",
+  storageBucket: "line-clone-5482a.appspot.com",
+  messagingSenderId: "416311964675",
+  appId: "1:416311964675:web:f3a16ded23e07a75d18a36"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db, auth};
